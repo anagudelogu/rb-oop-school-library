13a1c52c9e9625bda992c9e3b8a8d0074f72eac0
Andres Agudelo <anagudelogu@gmail.com>
2022-07-25 14:34:16 -0500

    Create rentals_menu file, class and display method

--- lib/app.rb
+++ lib/app.rb
@@ -58,23 +58,3 @@ class App
     nil
   end
 end
-
-def create_rental_menu(app)
-  puts 'Please select a book from the following list by number:'
-  books = app.books
-  books.each_with_index { |book, index| puts "#{index}) Title: \"#{book.title}\" Author: #{book.author}" }
-  print "Enter the desired book:\t"
-  book_idx = gets.chomp.to_i
-
-  puts 'Please select a person from the following list by number, not ID:'
-  people = app.people
-  people.each_with_index { |p, index| puts "#{index}) [#{p.class}] Name: #{p.name}, ID: #{p.id}, Age: #{p.age}" }
-  print "Enter the person number:\t"
-  person_idx = gets.chomp.to_i
-
-  print "Please enter the date: [YYYY/MM/DD]:\t"
-  date = gets.chomp
-
-  app.create_rental(date, person_idx, book_idx)
-  puts 'Rental created succesfully!'
-end
