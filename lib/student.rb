48c985aae73a921283edd9b72a5b5f657189d511
Andres Agudelo <anagudelogu@gmail.com>
2022-07-26 16:37:48 -0500

    Modify Student class to handle JSON

--- lib/student.rb
+++ lib/student.rb
@@ -11,4 +11,18 @@ class Student < Person
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'age' => @age,
+      'name' => @name,
+      'parent_permission' => @parent_permission
+    }.to_json(*args)
+  end
+
+  def self.json_create(student)
+    new(student['age'], student['name'], parent_permission: student['parent_permission'], id: student['id'].to_i)
+  end
 end
