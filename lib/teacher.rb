88f9aae8c325a25f50856bc2be8fa3cf36aeaa60
marurevi <maryrev@hotmail.com>
2022-07-26 18:35:14 -0300

    Modify Teacher class to create JSON file

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,12 +1,26 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown')
-    super(age, name)
+  def initialize(age, specialization, name = 'Unknown', id: nil)
+    super(age, name, id: id)
     @specialization = specialization
   end
 
   def can_use_service?
     true
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'age' => @age,
+      'name' => @name,
+      'specialization' => @specialization
+    }.to_json(*args)
+  end
+
+  def self.json_create(teacher)
+    new(teacher['age'], teacher['specialization'], teacher['name'], id: teacher['id'])
   end
 end
