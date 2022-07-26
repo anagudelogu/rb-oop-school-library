a33b7b7b1a29d56c63f6b984b4cff598045bdabc
marurevi <maryrev@hotmail.com>
2022-07-26 18:39:39 -0300

    Modify rental class to build JSON file

--- lib/rental.rb
+++ lib/rental.rb
@@ -9,4 +9,17 @@ class Rental
     @book = book
     @book.rentals << self
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'date' => @date,
+      'person' => @person,
+      'book' => @book
+    }.to_json(*args)
+  end
+
+  def self.json_create(rental)
+    new(rental['date'], rental['person'], rental['book'])
+  end
 end
